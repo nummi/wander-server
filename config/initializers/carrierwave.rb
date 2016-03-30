802e6ee163ae1d8a734a247eeacdac4c38f0e62f
Jerry Nummi <jerry@mutuallyhuman.com>
2016-03-30 08:52:52 -0700

    Wut

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,18 @@
 if ENV.has_key? 'AWS_ACCESS_KEY_ID'
+  Rails.logger.debug '----------------------------------------'
+  Rails.logger.debug ENV['AWS_ACCESS_KEY_ID']
+  Rails.logger.debug '----------------------------------------'
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIAJWFZXPPFUY6JPC7Q', # ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: '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', # ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'us-east-1' # ENV['AWS_REGION']
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION']
     }
 
     config.cache_dir      = "#{Rails.root}/tmp/uploads" 
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
-    config.fog_directory  = 'wander-free' # ENV['S3_BUCKET']
+    config.fog_directory  = ENV['S3_BUCKET']
   end
 else
   Rails.logger.warn "AWS_ACCESS_KEY_ID not found in ENV; CarrierWave is disabled."
