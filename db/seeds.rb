0d57784cc4dfa362e82b55acb5f2bd1b0ea859b9
Jerry Nummi <jerry@mutuallyhuman.com>
2016-04-04 15:33:05 -0400

    Comment out seeds to prevent stupid

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,95 +1,95 @@
-trip1 = Trip.create({ name: 'Spring 2016' })
-trip2 = Trip.create({ name: 'Portland' })
-
-Event.create([
-  {
-    publish: true,
-    cost: 0,
-    description: 'Taking off',
-    geolocation: { latitude: 41.681957, longitude: -81.28214 },
-    category: 'location',
-    weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
-  },
-  {
-    publish: true,
-    cost: 650,
-    description: 'This is a long description here',
-    geolocation: { latitude: 39.96819355055, longitude: -82.996259488017 },
-    category: 'location',
-    venue: { name: 'V Power Yoga' },
-    weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
-  },
-  {
-    publish: true,
-    cost: 4250,
-    geolocation: { latitude: 39.4394466, longitude: -87.4346486 },
-    category: 'expense',
-    venue: { name: 'Thorntons Gas Station' },
-    weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
-  },
-  {
-    publish: true,
-    cost: 0,
-    geolocation: { latitude: 39.4494446, longitude: -87.4346586 },
-    category: 'message',
-    description: 'Made it to our hotel safe and sound',
-    weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
-  },
-  {
-    publish: true,
-    cost: 3874,
-    geolocation: { latitude: 37.9815929, longitude: -91.7215187 },
-    category: 'expense',
-    venue: { name: 'Loves Gas Station' },
-    weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
-  },
-  {
-    publish: true,
-    cost: 2000,
-    geolocation: { latitude: 36.8761847, longitude: -94.9924853 },
-    category: 'expense',
-    venue: { name: 'Pump \'n Pete\'s' },
-    weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
-  },
-  {
-    publish: true,
-    cost: 3924,
-    geolocation: { latitude: 35.4824051, longitude: -97.6192804 },
-    category: 'expense',
-    venue: { name: 'Cirkle K Gas' },
-    weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
-  },
-  {
-    publish: true,
-    cost: 4155,
-    geolocation: { latitude: 34.9576045, longitude: -104.7680747 },
-    category: 'expense',
-    venue: { name: 'Shell' },
-    weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
-  },
-  {
-    publish: true,
-    cost: 2000,
-    geolocation: { latitude: 35.2612445, longitude: -105.3365887 },
-    category: 'expense',
-    venue: { name: 'Villanueva State Park' },
-    weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
-  },
-  {
-    publish: true,
-    cost: 0,
-    geolocation: { latitude: 36.0378636, longitude: -112.1178674 },
-    category: 'location',
-    venue: { name: 'Grand Canyon' },
-    weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
-  },
-  # {
-  #   publish: true,
-  #   cost: 0,
-  #   description: 'Cancelled flight, hotel provided by airline',
-  #   geolocation: { latitude: 35.1591247, longitude: -80.8869856 },
-  #   category: 'location',
-  #   venue: { name: 'Hawthorn Suites by Wyndham' },
-  #   weather: { 'cloudcover': '0', 'FeelsLikeC': '7', 'FeelsLikeF': '44', 'humidity': '27', 'observation_time': '10:49 PM', 'precipMM': '0.0', 'pressure': '1020', 'temp_C': '8', 'temp_F': '46', 'visibility': '16', 'weatherCode': '113', 'weatherDesc': [ { 'value': 'Clear' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0008_clear_sky_night.png' } ], 'winddir16Point': 'S', 'winddirDegree': '170', 'windspeedKmph': '7', 'windspeedMiles': '4' }
-  # }
-])
+# trip1 = Trip.create({ name: 'Spring 2016' })
+# trip2 = Trip.create({ name: 'Portland' })
+#
+# Event.create([
+#   {
+#     publish: true,
+#     cost: 0,
+#     description: 'Taking off',
+#     geolocation: { latitude: 41.681957, longitude: -81.28214 },
+#     category: 'location',
+#     weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
+#   },
+#   {
+#     publish: true,
+#     cost: 650,
+#     description: 'This is a long description here',
+#     geolocation: { latitude: 39.96819355055, longitude: -82.996259488017 },
+#     category: 'location',
+#     venue: { name: 'V Power Yoga' },
+#     weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
+#   },
+#   {
+#     publish: true,
+#     cost: 4250,
+#     geolocation: { latitude: 39.4394466, longitude: -87.4346486 },
+#     category: 'expense',
+#     venue: { name: 'Thorntons Gas Station' },
+#     weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
+#   },
+#   {
+#     publish: true,
+#     cost: 0,
+#     geolocation: { latitude: 39.4494446, longitude: -87.4346586 },
+#     category: 'message',
+#     description: 'Made it to our hotel safe and sound',
+#     weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
+#   },
+#   {
+#     publish: true,
+#     cost: 3874,
+#     geolocation: { latitude: 37.9815929, longitude: -91.7215187 },
+#     category: 'expense',
+#     venue: { name: 'Loves Gas Station' },
+#     weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
+#   },
+#   {
+#     publish: true,
+#     cost: 2000,
+#     geolocation: { latitude: 36.8761847, longitude: -94.9924853 },
+#     category: 'expense',
+#     venue: { name: 'Pump \'n Pete\'s' },
+#     weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
+#   },
+#   {
+#     publish: true,
+#     cost: 3924,
+#     geolocation: { latitude: 35.4824051, longitude: -97.6192804 },
+#     category: 'expense',
+#     venue: { name: 'Cirkle K Gas' },
+#     weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
+#   },
+#   {
+#     publish: true,
+#     cost: 4155,
+#     geolocation: { latitude: 34.9576045, longitude: -104.7680747 },
+#     category: 'expense',
+#     venue: { name: 'Shell' },
+#     weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
+#   },
+#   {
+#     publish: true,
+#     cost: 2000,
+#     geolocation: { latitude: 35.2612445, longitude: -105.3365887 },
+#     category: 'expense',
+#     venue: { name: 'Villanueva State Park' },
+#     weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
+#   },
+#   {
+#     publish: true,
+#     cost: 0,
+#     geolocation: { latitude: 36.0378636, longitude: -112.1178674 },
+#     category: 'location',
+#     venue: { name: 'Grand Canyon' },
+#     weather: { 'cloudcover': '75', 'FeelsLikeC': '-9', 'FeelsLikeF': '15', 'humidity': '63', 'observation_time': '07:06 PM', 'precipMM': '0.0', 'pressure': '1018', 'temp_C': '-3', 'temp_F': '27', 'visibility': '16', 'weatherCode': '116', 'weatherDesc': [ { 'value': 'Partly Cloudy' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ], 'winddir16Point': 'ENE', 'winddirDegree': '60', 'windspeedKmph': '24', 'windspeedMiles': '15' }
+#   },
+#   # {
+#   #   publish: true,
+#   #   cost: 0,
+#   #   description: 'Cancelled flight, hotel provided by airline',
+#   #   geolocation: { latitude: 35.1591247, longitude: -80.8869856 },
+#   #   category: 'location',
+#   #   venue: { name: 'Hawthorn Suites by Wyndham' },
+#   #   weather: { 'cloudcover': '0', 'FeelsLikeC': '7', 'FeelsLikeF': '44', 'humidity': '27', 'observation_time': '10:49 PM', 'precipMM': '0.0', 'pressure': '1020', 'temp_C': '8', 'temp_F': '46', 'visibility': '16', 'weatherCode': '113', 'weatherDesc': [ { 'value': 'Clear' } ], 'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0008_clear_sky_night.png' } ], 'winddir16Point': 'S', 'winddirDegree': '170', 'windspeedKmph': '7', 'windspeedMiles': '4' }
+#   # }
+# ])
